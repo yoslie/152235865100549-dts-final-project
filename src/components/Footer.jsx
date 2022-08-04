6ebb2959576038afc854a0cfb0f8fe922a732486
Yoslie <yoslie.tan@gmail.com>
2022-08-04 21:20:33 +0700

    refactor: refactor login&register; wording footer;

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-semiBlack-200" aria-labelledby="footer-heading">
       <div className="max-w-8xl mx-auto lg:mx-8">
         <p className="p-8 text-base text-gray-600 md:mt-0 md:order-1">
-          &copy; 2022 Masak - Yo! by Yoslie, All rights reserved.
+          &copy; 2022 Kulineran - Yo! by Yoslie, All rights reserved.
         </p>
       </div>
     </footer>
--- src/components/LoginOrRegisterForm.jsx
+++ src/components/LoginOrRegisterForm.jsx
@@ -64,61 +64,81 @@ const LoginOrRegisterForm = ({ loginOrRegister }) => {
   );
 
   return (
-    <div className="min-h-full flex">
-      <div className="hidden lg:block relative w-0 flex-1">
+    <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <img
-          className="absolute inset-0 h-full w-full object-cover"
-          src={process.env.PUBLIC_URL+"assets/images/login/ProfilePicture.svg"}
-          alt="Login Images Default"
+          className="mx-auto h-16 w-auto"
+          src={process.env.PUBLIC_URL + "assets/image/icons8-restaurant-building-100.png"}
+          alt="Restaurant Logo"
         />
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          {loginOrRegister === "login" ? "Sign in" : "Sign up"} to your account
+        </h2>
       </div>
-      <div className="bg-black flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-        <div className="mx-auto w-full max-w-sm lg:w-96">
-          <div className="mt-6">
-            <div className="space-y-6">
-              <div>
+
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <div className="space-y-6">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                Email address
+              </label>
+              <div className="mt-1">
                 <input
                   id="email"
                   name="email"
                   type="email"
                   autoComplete="email"
-                  placeholder="EMAIL"
                   required
-                  className="appearance-none block w-full px-3 py-4 border border-gray-700 text-midGray bg-black opacity-7 rounded-md sm:text-sm"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                   value={credential.email}
                   onChange={textFieldEmailOnChangeHandler}
                 />
               </div>
+            </div>
 
-              <div className="mt-2">
+            <div>
+              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                Password
+              </label>
+              <div className="mt-1">
                 <input
                   id="password"
                   name="password"
                   type="password"
                   autoComplete="current-password"
-                  placeholder="PASSWORD"
                   required
-                  className="appearance-none block w-full px-3 py-4 border border-gray-700 text-midGray bg-black opacity-7 rounded-md sm:text-sm"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                   value={credential.password}
                   onChange={textFieldPasswordOnChangeHandler}
                 />
               </div>
+            </div>
 
-              <div className="mt-2">
-                <button
-                  type="submit"
-                  className="w-full flex justify-center py-4 px-4 border border-transparent rounded-md text-sm font-medium text-white bg-red-900"
-                  onClick={buttonLoginOrRegisterOnClickHandler}
-                >
-                  {loginOrRegister === "login" ? "LOGIN" : "SIGN UP"}
-                </button>
+            <div>
+              <button
+                type="submit"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                onClick={buttonLoginOrRegisterOnClickHandler}
+              >
+                { loginOrRegister === "login" ? "Sign in" : "Sign up" }
+              </button>
+            </div>
+          </div>
+
+          <div className="mt-6">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-300" />
+              </div>
+              <div className="relative flex justify-center text-sm">
                 {loginOrRegister === "login" ? (
                   <Link to="/register">
-                    <div className="w-full flex justify-center mt-1 text-gray-500">or do you want Register ?</div>
+                    <span className="px-2 bg-white text-gray-500">or do you want Register ?</span>
                   </Link>
                 ) : (
                   <Link to="/login">
-                    <div className="w-full flex justify-center mt-1 text-gray-500">or do you want Login ?</div>
+                    <span className="px-2 bg-white text-gray-500">or do you want Login ?</span>
                   </Link>
                 )}
               </div>
